5d0db533a3e5003d582b7ced1683a1892f382876
mhasse <mhasse@d8321573-c53d-0410-acbf-c2a0f6c1541a>
2012-08-26 21:13:45 +0000

    Patch for PCI bursting issues.

---
+++ patch_02/header.asm
@@ -0,0 +1,219 @@
+      COMMENT *
+
+Defines for DSP register addresses.
+
+See info.asm for versioning and authors.
+
+	*
+	PAGE    132     ; Printronix page width - 132 columns
+	OPT	CEX	; print DC evaluations
+
+; Equates to define the X: memory tables
+VAR_TBL		EQU	0	; Variables and constants table
+
+APPLICATION	EQU	$800	; application memory start location in P memory
+				; note applications should start with this address 
+				; and end with a JMP to PACKET_IN
+				; if only want appl to run once
+				; penultimate line of code should be 
+				; to clear bit APPLICATION_LOADED in STATUS
+				; otherwise will run continusly until 'STP'
+				; command is sent
+
+APPL_PARAM	EQU	$200 	; application parameters in x memory start here.
+
+
+HF_FIFO		EQU	512	; number of 16 bit words in a half full FIFO
+SMALL_BLK	EQU	32	; small block burst size for < 512 pixels
+
+;;; Y memory (buffer) mapping	
+IMAGE_BUFFER	EQU	0	; Data frame buffer offset.
+REPLY_BUFFER	EQU     $100000	; Buffer MCE replies at 1M
+COMMAND_BUFFER	EQU	$200000	; Buffer MCE commands at 2M
+TIMER_BUFFER    EQU     $201000
+TIMER_BUFFER_END EQU    $202000
+
+	
+; HST timeout recovery....
+
+MAX_DUMP		EQU	512	; if HST timeout.. max number that could be in FIFO is 511..
+DUMP_BUFF		EQU	$1000	; store in Y memory above normal data buffer: in off-chip RAM
+
+
+
+; Various addressing control registers
+BCR	EQU	$FFFFFB		; Bus Control Register
+DCR	EQU	$FFFFFA		; DRAM Control Register
+AAR0	EQU	$FFFFF9		; Address Attribute Register, channel 0	
+AAR1	EQU	$FFFFF8		; Address Attribute Register, channel 1	
+AAR2	EQU	$FFFFF7		; Address Attribute Register, channel 2	
+AAR3	EQU	$FFFFF6		; Address Attribute Register, channel 3	
+PCTL	EQU	$FFFFFD		; PLL control register
+IPRP	EQU	$FFFFFE		; Interrupt Priority register - Peripheral
+IPRC	EQU	$FFFFFF		; Interrupt Priority register - Core
+
+; PCI control register
+DTXS	EQU	$FFFFCD		; DSP Slave transmit data FIFO
+DTXM	EQU	$FFFFCC		; DSP Master transmit data FIFO
+DRXR	EQU	$FFFFCB		; DSP Receive data FIFO
+DPSR	EQU	$FFFFCA		; DSP PCI Status Register 
+DSR	EQU	$FFFFC9		; DSP Status Register
+DPAR	EQU	$FFFFC8		; DSP PCI Address Register
+DPMC	EQU	$FFFFC7		; DSP PCI Master Control Register 
+DPCR	EQU	$FFFFC6		; DSP PCI Control Register
+DCTR	EQU	$FFFFC5		; DSP Control Register
+
+; Port E is the Synchronous Communications Interface (SCI) port
+PCRE	EQU	$FFFF9F		; Port Control Register
+PRRE	EQU	$FFFF9E		; Port Direction Register
+PDRE	EQU	$FFFF9D		; Port Data Register
+
+; Various PCI register bit equates
+STRQ	EQU	1		; Slave transmit data request (DSR)
+SRRQ	EQU	2		; Slave receive data request (DSR) 
+HACT	EQU	23		; Host active, low true (DSR)
+MTRQ	EQU	1		; Set whem master transmitter is not full (DPSR)
+MARQ	EQU	4		; Master address request (DPSR)
+MRRQ	EQU	2		; Master Receive Request (DPSR)
+TRTY	EQU	10		; PCI Target Retry (DPSR)
+
+APER	EQU	5		; Address parity error
+DPER	EQU	6		; Data parity error
+MAB	EQU	7		; Master Abort
+TAB	EQU	8		; Target Abort
+TDIS	EQU	9		; Target Disconnect
+TO	EQU	11		; Timeout
+MDT	EQU	14		; Master Data Transfer complete
+RDCQ	EQU	15		; Remaining Data Count Qualifier
+	
+SCLK	EQU	2		; SCLK = transmitter special code
+
+; bits in DPMC
+
+FC1	EQU	23
+FC0	EQU	22
+
+
+; DMA register definitions
+DSR0	EQU	$FFFFEF		; Source address register
+DDR0	EQU	$FFFFEE		; Destination address register
+DCO0	EQU	$FFFFED		; Counter register
+DCR0	EQU	$FFFFEC		; Control register
+
+; DCTR bits
+DCTR_HCIE	EQU	0 	; Interrupt enable
+DCTR_HF3	EQU	3	; Semaphore for INTA handshaking
+DCTR_HF4  	EQU	4	; 
+DCTR_HF5  	EQU	5	; 
+INTA	   	EQU	6	; Request PCI interrupt
+
+; The DSR host flags are written by the PCI host and read by the DSP
+DSR_HF0   EQU	3		; PC side INTA hand-shaking
+DSR_HF1   EQU	4		; PC side hand-shaking enabled
+DSR_HF2   EQU	5		; PC side INTA disable (polling mode)
+
+; DPCR bit definitions
+CLRT	EQU	14		; Clear transmitter
+MACE	EQU	18		; Master access counter enable
+IAE	EQU	21		; Insert Address Enable
+
+
+	
+; Addresses of ESSI port
+TX00	EQU	$FFFFBC		; Transmit Data Register 0
+SSISR0	EQU	$FFFFB7		; Status Register
+CRB0	EQU	$FFFFB6		; Control Register B
+CRA0	EQU	$FFFFB5		; Control Register A
+
+; SSI Control Register A Bit Flags
+TDE	EQU	6		; Set when transmitter data register is empty
+
+; Miscellaneous addresses
+RDFIFO	EQU	$FFFFFF		; Read the FIFO for incoming fiber optic data
+
+; Timer registers
+TCSR0	EQU	$FFFF8F		; Control and status register
+TLR0	EQU	$FFFF8E		; Load register
+TCPR0	EQU	$FFFF8D		; Compare register
+TCR0	EQU	$FFFF8C		; Count register
+TCSR1	EQU	$FFFF8B		; Control and status register
+TLR1	EQU	$FFFF8A		; Load register
+TCPR1	EQU	$FFFF89		; Compare register
+TCR1	EQU	$FFFF88		; Count register
+TCSR2	EQU	$FFFF87		; Control and status register
+TLR2	EQU	$FFFF86		; Load register
+TCPR2	EQU	$FFFF85		; Compare register
+TCR2	EQU	$FFFF84		; Count register
+
+;***************************************************************
+; Phase Locked Loop initialization
+PLL_INIT EQU	$050003		; PLL = 25 MHz x 4 = 100 MHz
+;****************************************************************
+
+; Port C is Enhanced Synchronous Serial Port 0
+PCRC	EQU	$FFFFBF		; Port C Control Register
+PRRC	EQU	$FFFFBE		; Port C Data direction Register
+PDRC	EQU	$FFFFBD		; Port C GPIO Data Register
+
+; Port D is Enhanced Synchronous Serial Port 1
+PCRD	EQU	$FFFFAF		; Port D Control Register
+PRRD	EQU	$FFFFAE		; Port D Data direction Register
+PDRD	EQU	$FFFFAD		; Port D GPIO Data Register
+
+; Bit number definitions of GPIO pins on Port C
+ROM_FIFO EQU	2		; Select ROM or FIFO accesses for AA1
+AUX1	EQU	4		; enable/disable byte swapping
+
+; Bit number definitions of GPIO pins on Port D
+EF	EQU	0		; FIFO Empty flag, low true
+HF	EQU	1		; FIFO half full flag, low true
+RS	EQU	2		; FIFO reset signal, low true
+FSYNC	EQU	3		; High during image transmission
+WRFIFO	EQU	5		; Low true if FIFO is being written to
+
+
+; Errors - self test application
+
+Y_MEM_ER	EQU	0	; y memory corrupted
+X_MEM_ER	EQU	1	; x memory corrupted
+P_MEM_ER	EQU	2	; p memory corrupted
+FO_EMPTY	EQU	3	; no transmitted data in FIFO
+
+FO_OVER		EQU	4	; too much data received
+FO_UNDER	EQU	5 	; not enough data receiv
+FO_RX_ER	EQU	6	; received data in FIFO incorrect.
+DEBUG		EQU	7	; debug bit
+
+
+;;; Timer TCSR bit definitions
+TE		EQU	0
+TOIE		EQU	1
+TCIE		EQU	2
+TOF		EQU	20
+TCF		EQU	21
+
+
+;;; FO transmitter memory-mapped location
+
+FO_SEND		EQU	$FFF000
+
+;--------------------------------------------------------------------
+; Interrupt configuration
+;--------------------------------------------------------------------
+;  IPRC determines core interrupt modes and levels.
+;   - [5:3] IRQB mode|level - FIFO half full
+;   - [8:6] IRQC mode|level - reset switch
+;  So $1C0 is 111|000|000 = IRQC level-triggered priority 3 and IRQB disabled.
+;
+;  IPRP determines peripheral interrupt levels.
+;   - [1:0] HI-32 level.  Must be higher than SR IPL mask
+;  So set to 2, and ensure SR[I] > 1.
+;
+;  SR determines many things... but most importantly
+;   - [9:8] Interrupt mask - must be smaller than HI-32 IPL
+;  So set to $100
+
+MY_IPRC		EQU	$0001C0
+MY_IPRP		EQU	$000002
+MY_SR		EQU	$000100
+
---
+++ patch_02/vars.asm
@@ -0,0 +1,208 @@
+      COMMENT *
+
+Variable table and bit defines for our variables.
+
+See info.asm for versioning and authors.
+
+	*
+
+
+; The variable table is mapped to X memory but stored inline in the
+; eeprom / P memory after the main code (but before the application
+; area).
+
+        ORG     X:VAR_TBL,P:
+
+
+	IF	@SCP("DOWNLOAD","ROM")	; Boot ROM code
+VAR_TBL_START	EQU	@LCV(L)-2
+	ENDIF
+
+	IF	@SCP("DOWNLOAD","ONCE")	; Download via ONCE debugger
+VAR_TBL_START	EQU	@LCV(L)
+	ENDIF
+
+
+;-----------------------------------------------
+STATUS			DC	0	; Internal status flags
+MODE			DC	0	; Operating mode control
+FRAME_COUNT		DC	0	; Count of data frames from MCE
+
+;-----------------------------------------------
+REV_NUMBER		DC	$550106	; byte 0 = minor revision #
+					; byte 1 = major revision #
+					; byte 2 = release Version (ascii letter)
+REV_DATA		DC	$000000 ; Not used by UBC
+P_CHECKSUM		DC	$2EF490 ; Not used by UBC
+
+;-----------------------------------------------
+NUM_DUMPED		DC	0	; number of words (16-bit) dumped to Y memory (512) after an HST timeout.
+;-----------------------------------------------
+
+
+;;; Vector command handling
+	
+DRXR_WD1		DC	0	; Storage for words read from PC during vector command
+DRXR_WD2		DC	0
+DRXR_WD3		DC	0
+DRXR_WD4		DC	0
+
+DTXS_WD1		DC	0	; Storage for words to be written to PC as reply
+DTXS_WD2		DC	0
+DTXS_WD3		DC	0
+DTXS_WD4		DC	0
+
+	
+;;; Register storage during vector command interrupts
+
+SV_A0			DC	0   
+SV_A1			DC	0 
+SV_A2			DC	0
+SV_B0			DC	0
+SV_B1			DC	0
+SV_B2			DC	0
+SV_X0			DC	0
+SV_X1			DC	0
+SV_Y0			DC	0
+SV_Y1			DC	0
+SV_R0			DC	0
+SV_SR			DC	0
+
+
+;;; MCE packet header storage
+
+HEAD_W1_0		DC	0       ; Preamble $A5A5
+HEAD_W1_1		DC	0	;          $A5A5
+HEAD_W2_0		DC	0	;          $5A5A
+HEAD_W2_1		DC	0	;          $5A5A
+HEAD_W3_0		DC	0	; 'RP' or 'DA'
+HEAD_W3_1 		DC	0	; '  '   $2020
+HEAD_W4_0		DC	0	; Packet size LSW
+HEAD_W4_1		DC	0	;             MSW
+
+
+;;; Packet processing
+	
+PACKET_SIZE		DC	0	; Size, in dwords of most recent packet from MCE.
+TOTAL_BUFFS		DC	0	; Number of 512 word half-buffers in packet.
+LEFT_TO_READ		DC	0	; Number of words left to read after last 512 buffer
+
+PREAMBLE_ERRORS		DC	0	; Failed on preamble processing
+PTYPE_ERRORS		DC	0	; Failed on packet type
+PSIZE_ERRORS		DC	0	; Failed on packet size test
+
+	
+;;; PCI burst parameters
+	
+PCI_BURST_SIZE		DC	$40	; Should be < 4*latency assigned by OS
+BURST_SIZE		DC	0
+BLOCK_SIZE		DC	0
+
+CON_SRC_LO		DC	0	; Set by CON host command
+CON_SRC_HI		DC	0
+
+YMEM_SRC		DC	0	; Vars for YMEM -> PC transfers
+BURST_DEST_LO		DC	0
+BURST_DEST_HI		DC	0
+
+BURST_SRC_LO		DC	0	; Vars for PC -> YMEM transfers
+BURST_SRC_HI		DC	0
+YMEM_DEST               DC      0
+	
+DMA_ERRORS		DC	0	; Error counting
+EC_TRTY			DC	0
+EC_TO			DC	0
+EC_TDIS			DC	0
+EC_TAB			DC	0
+EC_MAB			DC	0
+EC_DPER			DC	0
+EC_APER			DC	0
+
+	
+;;; Quiet Transfer Mode setup - for data packets -> PC
+	
+QT_BASE_LO		DC	0	; PC buffer start address bits 15-0
+QT_BASE_HI		DC	0	; PC buffer start address bits 31-16
+QT_BUF_SIZE		DC	0	; Separation of buffers, in bytes
+QT_BUF_MAX		DC	0	; Number of buffers
+QT_FRAME_SIZE		DC	0	; Expected data packet size, in bytes
+QT_INFORM		DC	0	; Number of packets to copy before informing
+
+QT_BUF_HEAD		DC	0	; Index of buf for next write
+QT_BUF_TAIL		DC	0	; Index at which we must not write
+
+QT_DEST_LO		DC	0	; PC address for next write
+QT_DEST_HI		DC	0	; 
+QT_INFORM_IDX		DC	0	; Number of packets since last inform
+QT_DROPS		DC	0	; Dropped packet count
+
+
+;;; Quiet-RP setup - for rapid replies -> PC
+	
+RP_BASE_LO		DC	0	; PC buffer start address
+RP_BASE_HI		DC	0	; 
+RP_MAX_SIZE		DC	0	; Maximum reply size, dwords
+RP_DROPS		DC	0	; Dropped packet count
+
+
+;;; Timer storage buffer index
+
+TIMER_INDEX		DC	0
+
+	
+;;; Generic debug
+		
+BDEBUG0			DC	0
+BDEBUG1			DC	0
+BDEBUG2			DC	0
+BDEBUG3			DC	0
+BDEBUG4			DC	0
+BDEBUG5			DC	0
+BDEBUG6			DC	0
+BDEBUG7			DC	0
+BDEBUG8			DC	0
+BDEBUG9			DC	0
+
+;----------------------------------------------------------
+
+;;; Bit defines for STATUS word
+
+APPLICATION_RUNNING	EQU	0   ; Indicates application is in progress
+SEND_TO_HOST		EQU	1   ; set in HST ISR when host ready for packet (stays set until after HST reply)
+FATAL_ERROR		EQU	2   ; PCI message to host error detected by driver....
+FO_WRD_RCV		EQU	3   ; set when packet detected in FIFO - stays set till packet processed
+
+HST_NFYD		EQU	9   ; set after host notified (NFY message) of packet (stays set until after HST reply)
+
+CON_DEMAND		EQU	10  ; Host has requested an MCE command be sent
+CON_MCE                 EQU     11  ; Command has been copied to Y buffer and should be sent to MCE
+
+PCIDMA_RESTART		EQU	16  ; DMA flags used for error recovery
+PCIDMA_RESUME		EQU	17
+
+QT_FLUSH		EQU	20  ; Set when it is time to inform Host of current buffer position.
+RP_BUFFER_FULL		EQU	21  ; Set when Quiet RP buffer is occupied.
+
+FREEZER			EQU	22  ; Suspend operations and just idle in the main loop
+MAIN_LOOP_POLL          EQU     23  ; Cleared by the main loop, use to check for DSP lock-up
+
+
+;;; Bit defines for MODE word
+
+MODE_APPLICATION	EQU	0   ; set if PCI application to run
+MODE_MCE		EQU	1   ; process packets from MCE (!choke)
+MODE_QT			EQU	2   ; Quiet transfer for data packets (QT mode)
+MODE_RP_BUFFER		EQU     3   ; Quiet transfer for reply packets (Quiet-RP)
+
+
+;;; END OF VARIABLE TABLE
+	
+	IF	@SCP("DOWNLOAD","ROM")	; Boot ROM code
+VAR_TBL_END	EQU	@LCV(L)-2
+	ENDIF
+
+	IF	@SCP("DOWNLOAD","ONCE")	; Download via ONCE debugger
+VAR_TBL_END	EQU	@LCV(L)
+	ENDIF
+
+VAR_TBL_LENGTH EQU	VAR_TBL_END-VAR_TBL_START
